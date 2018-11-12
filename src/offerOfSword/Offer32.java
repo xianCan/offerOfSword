2d44b09c69e14bf8aa74ba30ecb4f0b27adc38a6
xianCan <2492716147@qq.com>
2018-11-12 23:42:37 +0800

    剑指Offer第32题：二叉树的深度

---
+++ src/offerOfSword/Offer32.java
@@ -0,0 +1,24 @@
+package offerOfSword;
+
+/**
+ * 二叉树的深度
+ */
+
+public class Offer32 {
+    public int TreeDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    class TreeNode{
+        int val=0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+}
