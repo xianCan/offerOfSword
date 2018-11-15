db37404e957434c8048e276d11d75a723f5473f8
xianCan <2492716147@qq.com>
2018-11-15 15:07:30 +0800

    剑指Offer第37题：约瑟夫环

---
+++ src/offerOfSword/Offer37.java
@@ -0,0 +1,35 @@
+package offerOfSword;
+
+import java.util.LinkedList;
+
+/**
+ * Created by xianCan on 2018/11/15 14:23
+ *
+ * 约瑟夫环
+ */
+public class Offer37 {
+    /**
+     *  下表从0开始数
+     * @param n 总人数
+     * @param m 数到第几个出列
+     * @return
+     */
+    public static int LastRemaining_Solution(int n, int m) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=1; i<=n; i++){
+            list.add(i);
+        }
+        int bt = 0;
+        while(list.size()>1){
+            //第一次删掉的位置是从0开始数m-1个位置，以后每次从删掉的下一个节点开始取，
+            // 所以每次要在bt的索引处加上m-1，因为是环，所以加了以后对链表长度取余
+            bt = (bt+m) % list.size();
+            list.remove(bt);
+        }
+        return list.size() == 1 ? list.get(0) : 0;
+    }
+    //测试
+    public static void main(String[] args){
+        System.out.println(LastRemaining_Solution(10,5));
+    }
+}
