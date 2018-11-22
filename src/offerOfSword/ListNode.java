1acb57df441996045332024d7dc1c404fc0aca96
xianCan <2492716147@qq.com>
2018-11-22 13:58:40 +0800

    公用链表节点

---
+++ src/offerOfSword/ListNode.java
@@ -0,0 +1,15 @@
+package offerOfSword;
+
+/**
+ * @authod xianCan
+ * @date 2018/11/22 13:56
+ *
+ * 公用链表节点
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
