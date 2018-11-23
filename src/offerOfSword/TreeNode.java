2309dc120c534a5fe3212768fe266c191f286215
xianCan <2492716147@qq.com>
2018-11-23 09:32:53 +0800

    公用树节点

---
+++ src/offerOfSword/TreeNode.java
@@ -0,0 +1,16 @@
+package offerOfSword;
+
+/**
+ * @authod xianCan
+ * @date 2018/11/23 9:30
+ *
+ * 公用树节点
+ */
+public class TreeNode {
+    int value=0;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int value){
+        this.value=value;
+    }
+}
